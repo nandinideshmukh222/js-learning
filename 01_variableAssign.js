5e9f1806d400aaf46cc8d7a8a266c1c046cdb6a2
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-03-08 18:23:53 +0530

    variable assignment
    variable assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,29 @@
+console.log("======step1========");
+var myFullName;
+console.log("My FullName variable value before Initialization:",myFullName);
+myFullName="Komal Nimbalkar";
+console.log("My Full Name variable value after initialization:",myFullName);
+myFullName="Komal Pravin Nimbalkar";
+console.log("My full Name after updation:",myFullName);
+
+
+console.log("========step2===========")
+var myLove="I love only JS";
+console.log(myLove);
+
+
+console.log("========step3================");
+var firstName="Komal";
+console.log("First-Name:",firstName);
+var lastName="Nimbalkar";
+console.log("Last-Name:",lastName);
+var age=30;
+console.log("Age:",age);
+var address="Ivy estate ,Wagholi";
+console.log("Address:",address);
+var pinCode=412207;
+console.log("Pincode:",pinCode);
+ var gender="Female";
+ console.log("Gender:",gender);
+ var isMarried="Yes";
+ console.log("Is Married:",isMarried);
