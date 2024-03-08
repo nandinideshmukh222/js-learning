e59f41e6940f52338b8277f9db5cddd93a1661ed
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-03-08 18:22:00 +0530

    swap  variable assignment
    swap  variable assignment

---
+++ 01_swapVarAssign.js
@@ -0,0 +1,35 @@
+console.log("=======Step:01==========");
+var name1 ="Sweety";
+var name2 ="Cutie";
+
+console.log("======== Before Swap ==========");
+
+console.log("sweety:", name1, " cutie:", name2);
+
+console.log("======== After Swap ==========");
+ 
+var temp=name1;
+  name1=name2;
+  name2=temp;
+ console.log("sweety:",name1, " cutie:", name2);
+
+
+
+
+
+console.log("=======Step:02==========");
+
+var num1 = 100; 
+var num2 = 200; 
+var num3 =  300;
+
+console.log("======== Before Swap ==========");
+console.log("num1:", num1, " num2:", num2,"num3",num3);
+
+console.log("======== After Swap ==========");
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("num1:", num1, " num2:", num2,"num3",num3);
+
