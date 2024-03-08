6bb6baf4a79f406dd707581fa58e0a7759dd2d73
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-03-08 18:21:10 +0530

    hello word pgm
    hello word pgm

---
+++ helloWorld.js
@@ -0,0 +1,15 @@
+// Camel Case: test.js, hello.js, helloWorld.js, myFirstProgram.js
+// Kebab: test.js, hello.js, hello-world.js, my-first-program.js
+// Snake: test.js, hello.js, hello_world.js, my_first_program.js
+var city;
+city="pune";
+console.log(city);
+var age;
+age=30;
+console.log(age);
+var name;
+ name="Nandini";
+console.log(name)
+var sir_name;
+sir_name="Nimbalkar";
+console.log(sir_name);
