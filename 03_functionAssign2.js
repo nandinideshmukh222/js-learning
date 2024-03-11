6f8473819631ee80381d0bc75a5da30635556df2
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-03-11 15:30:47 +0530

    function assignment 02
    function assignment 02

---
+++ 03_functionAssign2.js
@@ -0,0 +1,19 @@
+function bankDetails(bankName,accountNum,location,pincode){
+    var result=bankName;
+    console.log("Bank Name :",result);
+    var result=accountNum;
+    console.log("Account Number :",result);
+    var result=location;
+    console.log("Location :",result);
+    var result=pincode;
+    console.log("Pincode:",result);
+    console.log("****************************");
+
+   
+   
+
+}
+    bankDetails("CITI Bank",34436657,"Pune",431202);
+    bankDetails("Axis Bank",46536644,"Mumbai",441202);
+    bankDetails("HDFC Bank",46536644,"Pune",441202,"Open");
+
