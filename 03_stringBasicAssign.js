3ea1b228c8c8f8851a2f025427fd6b9b689c6736
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-03-12 13:32:45 +0530

    String basics conacate andf  char
    String basics conacate andf  char

---
+++ 03_stringBasicAssign.js
@@ -0,0 +1,20 @@
+console.log("========step 1================");
+  function stringBasics(){
+    console.log(`My dream company is :" Microsoft"`);
+  }
+
+      stringBasics();
+
+ console.log("========step 2===============");
+    
+      function myHobbies(){ 
+    var  hobby1 ="playing sports game";
+    var  hobby2 ="Listening songs";
+    var  hobby3 ="Travelling";
+    var result=hobby1+hobby2 +hobby3;
+     console.log(`My Hobbies: ${hobby1} ${ hobby2} ${ hobby3}`);
+         var totalChars=result.length
+         console.log(`Total no. of Char: ${totalChars}`);
+
+     }
+        myHobbies();
