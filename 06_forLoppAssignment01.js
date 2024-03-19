d5a288bd499550340eb8d68c4cfeeb97c27baff9
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-03-19 14:24:29 +0530

    for loop asignment
    for loop asignment

---
+++ 06_forLoppAssignment01.js
@@ -0,0 +1,44 @@
+console.log(`// WAP to print numbers from 5 to 15`);
+
+for (let index = 15; index <= 24; index++) {
+   console.log(index);
+}
+
+
+console.log(`// WAP to print numbers from 50 to 40 decreamenting by 1`);
+
+for (let index = 50; index >= 40; index--) {
+   console.log(index);
+}
+
+
+
+console.log(`// WAp to find first 15 odd no`);
+
+for (let index =1; index <= 30; index=index+2) {
+   console.log(index);
+}
+
+console.log(`// WAP to find first 10 even no.`);
+
+for (let index =0; index <=20; index=index+2) {
+   console.log(index);
+}
+
+console.log(`// WAP to print table of 5`);
+
+for (let index =5; index <= 50; index=index+5) {
+   console.log(index);
+}
+
+console.log(`// WAP to print table of 10`);
+
+for (let index =10; index <= 100; index=index+10) {
+   console.log(index);
+}
+
+console.log(`// WAP to print table 10 in reverse order`);
+
+for (let index =100; index >= 10; index=index-10) {
+   console.log(index);
+}
